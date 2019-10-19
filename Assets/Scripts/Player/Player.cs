d55a7aef9e117c154cb4b026f72b241d953cb6d8
GreenRamSilgen <ps3iswow@gmail.com>
2019-10-19 12:49:30 -0700

    Player
    Add Player and movement with wasd keys.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10f;
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+        transform.position += moveDir * speed * Time.deltaTime;
+    }
+}
