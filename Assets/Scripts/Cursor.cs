6799b014568e26ce32b9a54f7e906603eaf82129
bryanly200 <bryanly200@gmail.com>
2019-10-19 12:51:54 -0700

    Update Cursor.cs

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Cursor : MonoBehaviour
 {
+
+    public Tilemap Layer1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +20,17 @@ public class Cursor : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.W)) {
             Move(Vector2.up);
         }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if (Input.GetKeyDown(KeyCode.A)) {
             Move(Vector2.left);
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+        if (Input.GetKeyDown(KeyCode.S)) {
             Move(Vector2.down);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+        if (Input.GetKeyDown(KeyCode.D)) {
             Move(Vector2.right);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Dig();
         }
 
     }
@@ -35,5 +38,10 @@ public class Cursor : MonoBehaviour
     public void Move(Vector2 direction)
     {
         transform.Translate(direction);
+    }
+
+    public void Dig()
+    {
+        Vector3 currentcell = 
     }
 }
