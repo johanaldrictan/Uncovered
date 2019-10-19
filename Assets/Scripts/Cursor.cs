bfadc7c4c049bf74671e9032318e4968856c7603
bryanly200 <bryanly200@gmail.com>
2019-10-19 12:38:45 -0700

    update

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W)) {
+            Move(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2.down);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2.right);
+        }
+
+    }
+
+    public void Move(Vector2 direction)
+    {
+        transform.Translate(direction);
+    }
+}
