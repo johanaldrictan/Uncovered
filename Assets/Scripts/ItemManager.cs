6c8f542ac4e19e16bbc6bf050bdfd919940a12a5
GreenRamSilgen <ps3iswow@gmail.com>
2019-10-20 13:44:20 -0700

    ItemFinding Hud
    --added item hud
    --need to add image and test animation

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    private bool item1Found;
+    private bool item2Found;
+    // Start is called before the first frame update
+    void Start()
+    {
+        item1Found = false;
+        item2Found = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
