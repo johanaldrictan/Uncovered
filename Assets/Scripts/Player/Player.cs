bc3394ecfd6221c660600b486f3f0e4ebbd27e50
Johan Tan <johan.tan21@gmail.com>
2019-10-20 13:39:42 -0700

    Fixed player movement code to rigidbody

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,55 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     private DialogueManager dialogueMan;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public float walkSpeed = 10f;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Awake()
     {
         dialogueMan = DialogueManager.FindObjectOfType<DialogueManager>();
     }
-        public Animator animator;
     // Update is called once per frame
     void Update()
     {
-        float speed = 10f;
-        float moveX = 0f;
-        float moveY = 0f;
-        if(dialogueMan.boxOn() == true)//STOP CHRACTER FROM MOVING IF DIALOGUE IS HAPPENING
+        if(dialogueMan.boxOn() != true)//STOP CHRACTER FROM MOVING IF DIALOGUE IS HAPPENING
         {
-
-        }
-        else
-        {
-            if(Input.GetKey(KeyCode.W))
+            if (Input.GetAxisRaw("Horizontal") != 0)
             {
-                moveY = +1f;
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = 0;
+            }
+            else if (Input.GetAxisRaw("Vertical") != 0)
+            {
+                movement.y = Input.GetAxisRaw("Vertical");
+                movement.x = 0;
             }
             else
-            if(Input.GetKey(KeyCode.S))
             {
-                moveY = -1f;
+                movement.y = 0;
+                movement.x = 0;
             }
-            else if(Input.GetKey(KeyCode.A))
-            {
-                moveX = -1f;
-            }
-            else if(Input.GetKey(KeyCode.D))
-            {
-                moveX = +1f;
-            }
+
+            animator.SetFloat("moveX", movement.x);
+            animator.SetFloat("moveY", movement.y);
         }
-        animator.SetFloat("moveX", moveX);
-        animator.SetFloat("moveY",moveY);
-        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
-        transform.position += moveDir * speed * Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        movement = movement.normalized;
+        rb.MovePosition(rb.position + movement * walkSpeed * Time.fixedDeltaTime);
     }
 
-    
+
 }
