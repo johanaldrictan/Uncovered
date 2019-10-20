994671258ad6d7c598115be016702e5944274087
Johan Tan <johan.tan21@gmail.com>
2019-10-20 05:49:23 -0700

    More Wwise Implementation
    Relocated audio files
    Added necessary objects to the Wwise project
    Set up the soundbank
    WARNING! DO NOT DELETE THE WWISE GLOBAL GAME OBJECT FROM YOUR SCENE

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUS_OVERWORLD = 3621165624U;
+        static const AkUniqueID PLAY_SFX_AMB_SEAGULLS = 1395785617U;
+        static const AkUniqueID PLAY_SFX_AMB_SECRET = 1251254653U;
+        static const AkUniqueID PLAY_SFX_COIN = 3833599577U;
+        static const AkUniqueID PLAY_SFX_EXCAV_BRUSH = 1701975916U;
+        static const AkUniqueID PLAY_SFX_EXCAV_DIG = 3121859900U;
+        static const AkUniqueID PLAY_SFX_EXCAV_FAIL = 4274094232U;
+        static const AkUniqueID PLAY_SFX_EXCAV_FOUND = 1570490760U;
+        static const AkUniqueID PLAY_SFX_OCEAN = 3409418128U;
+        static const AkUniqueID PLAY_SFX_WALK = 3902146761U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID STOP_MUS_OVERWORLD = 1492024762U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID UNCOVERED = 2407602878U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
